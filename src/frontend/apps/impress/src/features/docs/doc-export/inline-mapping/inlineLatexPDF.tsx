72aeda21493b5eb4a76ed815a2c058bd3beb2285
Timothée Danneels <dev.web.td@gmail.com>
2025-06-04 03:14:44 +0200

    added inline latex pdf

--- src/frontend/apps/impress/src/features/docs/doc-export/inline-mapping/inlineLatexPDF.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-export/inline-mapping/inlineLatexPDF.tsx
@@ -1,13 +1,22 @@
-import { Text, View } from '@react-pdf/renderer';
+import { Image, View } from '@react-pdf/renderer';
+import tex2svg from 'tex-to-svg';
 
 import { DocsExporterPDF } from '../types';
+import { convertSvgToPng } from '../utils';
 
 export const blockMappingInlineLatexPDF: DocsExporterPDF['mappings']['inlineContentMapping']['inlineLatex'] =
   (content) => {
+    const svgString = tex2svg(content.props.formula);
+    const baseWidth = parseFloat(
+      svgString.match(/width="([^"]+)"/)?.[1].replace('ex', '') || '0',
+    );
+
+    // Convert SVG to PNG synchronously
+    const dataUri = convertSvgToPng(svgString, baseWidth * 30);
+
     return (
-      //TODO implement inlineLatex
-      <View wrap={false}>
-        <Text>{content.props.formula}</Text>
+      <View>
+        <Image src={dataUri} style={{ width: baseWidth * 10 }} />
       </View>
     );
   };
